4fd7f538969a344cdec8162046febeab9b54a5a4
Tim <jasper7466@yandex.ru>
2023-12-20 22:37:22 +0500

    feat: solve "common-character-count" problem

--- src/common-character-count.js
+++ src/common-character-count.js
@@ -11,11 +11,36 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For s1 = "aabcc" and s2 = "adcaa", the output should be 3
  * Strings have 3 common characters - 2 "a"s and 1 "c".
  */
-function getCommonCharacterCount(/* s1, s2 */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getCommonCharacterCount(s1, s2) {
+  const dict1 = {};
+  const dict2 = {};
+
+  // Make dicts for each string: key - character, value - count
+  countCharacters(s1, dict1);
+  countCharacters(s2, dict2);
+
+  // Choose the shortest dict to iterate over it (for reducing iterations count)
+  const [shortDict, longDict] =
+    Object.keys(dict1) < Object.keys(dict2) ? [dict1, dict2] : [dict2, dict1];
+
+  let commonCharactersCount = 0;
+
+  for (const [key, value] of Object.entries(shortDict)) {
+    const count = Math.min(value, longDict[key]);
+    if (count) {
+      commonCharactersCount += count;
+    }
+  }
+
+  return commonCharactersCount;
+}
+
+function countCharacters(str, dict) {
+  for (const character of str) {
+    dict[character] = dict[character] ? dict[character] + 1 : 1;
+  }
 }
 
 module.exports = {
-  getCommonCharacterCount
+  getCommonCharacterCount,
 };
